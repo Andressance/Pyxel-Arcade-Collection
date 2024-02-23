e6f6a63c8f705452d89efb247226875f9299b15c
Andressance <112200070+Andressance@users.noreply.github.com>
2024-02-23 08:58:41 +0100

    Update ball.py

--- pong/ball.py
+++ pong/ball.py
@@ -3,6 +3,7 @@ from random import randint
 
 class Ball:
     def __init__(self) -> None:
+        # Initialize ball position and speed
         self.x = 80
         self.y = 60
         self.speedX = -1.0 if randint(0, 1) == 0 else 1.0
@@ -11,17 +12,21 @@ class Ball:
         self.out_of_bounds = False
 
     def draw(self):
+        # Draw ball
         pyxel.circ(self.x, self.y, self.r, 7)
 
     def update(self):
+        # Update ball position
         self.x += self.speedX
         self.y += self.speedY
         
+        # Reflect ball if it hits top or bottom boundaries
         if self.y < 0 or self.y > 120:
             self.speedY *= -1
         
     
     def reset(self):
+        # Reset ball position and speed
         self.x = 80
         self.y = 60
         self.speedX = -1.0 if randint(0, 1) == 0 else 1.0
